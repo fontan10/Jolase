2697204ca327333b7b17528071c43bfaa5e971d0
fontan10 <josephfontanilla2@gmail.com>
2022-12-28 17:29:28 -0500

    TPTPGraph CRDT (#11)
    * Implement TPTPGraph and functional tests
    
    * Remove print statements

---
+++ MergeSharp.Tests/TPTPGraphTests.cs
@@ -0,0 +1,116 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MergeSharp.Tests;
+
+public class TPTPGraphTests
+{
+    [Fact]
+    public void SingleGraph()
+    {
+        TPTPGraph graph = new();
+        var v1 = Guid.NewGuid();
+        var v2 = Guid.NewGuid();
+        Assert.False(graph.AddEdge(v1, v2));
+        Assert.Empty(graph.LookupEdges());
+
+        graph.AddVertex(v1);
+        Assert.Equal(graph.LookupVertices(), new[] { v1 });
+
+        Assert.False(graph.AddEdge(v1, v2));
+        Assert.False(graph.AddEdge(v2, v1));
+
+        graph.AddVertex(v2);
+        Assert.Equal(graph.LookupVertices().ToHashSet(), new HashSet<Guid> { v1, v2 });
+
+        Assert.True(graph.AddEdge(v1, v2));
+        Assert.True(graph.AddEdge(v1, v2));
+        Assert.True(graph.AddEdge(v1, v1));
+        Assert.True(graph.AddEdge(v2, v1));
+
+        Assert.Equal(graph.LookupEdges().ToHashSet(),
+                        new HashSet<(Guid, Guid)> {
+                            (v1, v2),
+                            (v1, v1),
+                            (v2, v1)
+                         });
+
+        Assert.False(graph.RemoveVertex(v1));
+
+        Assert.False(graph.RemoveEdge(v2, v2));
+        Assert.True(graph.RemoveEdge(v1, v2));
+        Assert.False(graph.RemoveEdge(v1, v2));
+        Assert.True(graph.RemoveEdge(v2, v1));
+
+        Assert.False(graph.RemoveVertex(v1));
+        Assert.True(graph.RemoveVertex(v2));
+    }
+
+    [Fact]
+    public void MultipleGraphs1()
+    {
+        var v1 = Guid.NewGuid();
+        var v2 = Guid.NewGuid();
+
+        TPTPGraph first = new();
+        TPTPGraph second = new();
+
+        first.AddVertex(v1);
+        first.AddVertex(v2);
+
+        second.ApplySynchronizedUpdate(first.GetLastSynchronizedUpdate());
+
+        first.RemoveVertex(v1);
+        second.AddEdge(v1, v2);
+
+        Assert.Equal(new [] {v2}, first.LookupVertices());
+        Assert.Empty(first.LookupEdges());
+
+        Assert.Equal(new HashSet<Guid> {v1, v2}, second.LookupVertices().ToHashSet());
+        Assert.Equal(new [] {(v1, v2)}, second.LookupEdges());
+
+        first.ApplySynchronizedUpdate(second.GetLastSynchronizedUpdate());
+        second.ApplySynchronizedUpdate(first.GetLastSynchronizedUpdate());
+
+        Assert.Equal(first.LookupVertices(), second.LookupVertices());
+        Assert.Equal(first.LookupEdges(), second.LookupEdges());
+
+        Assert.Equal(new [] {v2}, first.LookupVertices());
+        Assert.Empty(first.LookupEdges());
+    }
+
+    [Fact]
+    public void MultipleGraphs2()
+    {
+        var v1 = Guid.NewGuid();
+        var v2 = Guid.NewGuid();
+
+        TPTPGraph first = new();
+        TPTPGraph second = new();
+
+        first.AddVertex(v1);
+        first.AddVertex(v2);
+
+        second.ApplySynchronizedUpdate(first.GetLastSynchronizedUpdate());
+
+        first.RemoveVertex(v1);
+        second.AddEdge(v1, v2);
+
+        first.ApplySynchronizedUpdate(second.GetLastSynchronizedUpdate());
+        second.ApplySynchronizedUpdate(first.GetLastSynchronizedUpdate());
+
+        second.AddVertex(v1);
+
+        first.ApplySynchronizedUpdate(second.GetLastSynchronizedUpdate());
+        second.ApplySynchronizedUpdate(first.GetLastSynchronizedUpdate());
+
+        Assert.Equal(first.LookupVertices(), second.LookupVertices());
+        Assert.Equal(first.LookupEdges(), second.LookupEdges());
+
+        Assert.Equal(new [] {v2}, first.LookupVertices());
+        Assert.Empty(first.LookupEdges());
+    }
+}
